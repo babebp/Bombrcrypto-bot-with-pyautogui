a47c3d88e5003cd022542675ac87117b2b97e356
Pacharawut Thanawut <tpacharawut@hotmail.com>
2022-01-30 06:43:00 +0700

    fix resting to rest all monitor

--- modules/rest_all.py
+++ modules/rest_all.py
@@ -1,19 +1,29 @@
 import pyautogui
 import time
 
+from datetime import datetime
+from find_monitor import finding_monitor
+
 class rest_all():
     def rest():
-        print('Status : Resting')
-        pyautogui.click(953, 707) # Arrow Up
-        time.sleep(2)
-        pyautogui.click(953, 683) # Heroes
-        time.sleep(1)
-        pyautogui.click(931, 291) # Rest All
-        time.sleep(1)
-        pyautogui.click(1012, 243) # Close Popup
-        time.sleep(1)
-        pyautogui.click(946, 464) # Close Arrow Popup
-        time.sleep(1)
-        pyautogui.click(516, 161) # Back to main menu
-        time.sleep(1)
-        print('Process : Resting for 1 hour')
+        monitor = finding_monitor.find_monitor()
+        for i in range(len(monitor)):
+            monitor[i].minimize()
+            monitor[i].maximize()
+            print(f'Status : Resting Monitor {i + 1}')
+            pyautogui.click(953, 707) # Arrow Up
+            time.sleep(2)
+            pyautogui.click(953, 683) # Heroes
+            time.sleep(1)
+            pyautogui.click(931, 291) # Rest All
+            time.sleep(1)
+            pyautogui.click(1012, 243) # Close Popup
+            time.sleep(1)
+            pyautogui.click(946, 464) # Close Arrow Popup
+            time.sleep(1)
+            pyautogui.click(516, 161) # Back to main menu
+            time.sleep(1)
+            
+
+            monitor[i].restore()
+            
