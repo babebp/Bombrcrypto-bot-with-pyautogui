0a80d755003581ba4f9933920048575a93d611e7
Pacharawut Thanawut <tpacharawut@hotmail.com>
2022-01-30 06:21:03 +0700

    adding rest_all coordinate and back to main page

--- modules/rest_all.py
+++ modules/rest_all.py
@@ -4,17 +4,16 @@ import time
 class start_treasure():
     def start():
         print('Status : Resting')
-        pyautogui.click(1910, 80) # Scroll Up
-        time.sleep(1)
-        pyautogui.click(1026, 559) # Treasure Mode
-        time.sleep(1)
         pyautogui.click(953, 707) # Arrow Up
         time.sleep(2)
         pyautogui.click(953, 683) # Heroes
         time.sleep(1)
-        pyautogui.click(870, 291) # Start All
+        pyautogui.click(931, 291) # Rest All
         time.sleep(1)
         pyautogui.click(1012, 243) # Close Popup
         time.sleep(1)
         pyautogui.click(946, 464) # Close Arrow Popup
         time.sleep(1)
+        pyautogui.click(516, 161) # Back to main menu
+        time.sleep(1)
+        print('Process : Resting for 1 hour')
