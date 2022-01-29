4d504381903ff24d97e1fd0daada3e0c1125ba93
Pacharawut Thanawut <tpacharawut@hotmail.com>
2022-01-30 06:43:33 +0700

    add more module for working

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from modules.find_monitor import finding_monitor
 from modules.start_treasure_mode import start_treasure
 from modules.count_down import counting_down
 from modules.rest_all import rest_all
+from modules.re_map import reset_map
 
 
 print('--------------------------------------')
@@ -20,28 +21,28 @@ monitor = finding_monitor.find_monitor()
 time.sleep(2)
 print(f'Process : Detected {len(monitor)} Bombcrypto')
 
-for i in range(len(monitor)):
-    monitor[i].minimize()
-    monitor[i].maximize()
+while True:
+    # Start Playing Each Monitor
+    for i in range(len(monitor)):
+        monitor[i].minimize()
+        monitor[i].maximize()
 
-    #Start Playing
-    now = datetime.now().strftime('%H:%M:%S')
-    print(f'Process : Start Treasure Mode at {now}')
-    start_treasure.start()
+        # Start Playing
+        now = datetime.now().strftime('%H:%M:%S')
+        print(f'Process : Start Treasure Mode at {now}')
+        start_treasure.start()
+        monitor[i].restore()
 
-    #Countdown
-    time.sleep(1800)
+    # Re-map
+    print('Process : Remap')
+    reset_map.re_map()
+
+    # Countdown
+    time.sleep(150)
 
     #Start Resting
+    print('Process : Resting for 1 hour')
     rest_all.rest()
 
-
-
-
-
-
-
-
-    
-
-
+    #Countdown
+    counting_down.start_count_down(delay_hr = 1, delay_min = 0, step = 'Start')
